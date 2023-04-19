7064008e677b6f704eb0e1489750dbfbb97f35f3
Vyacheslav <32650052+vyacheslavslava@users.noreply.github.com>
2023-04-19 21:03:58 +0300

    TLS_SIG AUTH

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -598,7 +598,7 @@ function installQuestions() {
 		echo "   1) tls-crypt (recommended)"
 		echo "   2) tls-auth"
 		until [[ $TLS_SIG =~ [1-2] ]]; do
-			read -rp "Control channel additional security mechanism [1-2]: " -e -i 1 TLS_SIG
+			read -rp "Control channel additional security mechanism [1-2]: " -e -i 2 TLS_SIG
 		done
 	fi
 	echo ""
